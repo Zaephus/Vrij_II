58fa26b8a8c52919071b1656309d865c5dc7e1a1
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-06-14 22:32:38 +0200

    Final vid fixes

--- Vrij_II/Assets/01_Scripts/Game States/EndState.cs
+++ Vrij_II/Assets/01_Scripts/Game States/EndState.cs
@@ -8,14 +8,21 @@ public class EndState : BaseState<GameManager> {
     [SerializeField]
     private GameObject endMenu;
 
+    [SerializeField]
+    private GameObject endVideo;
+
     public override void OnStart() {
-        endMenu.SetActive(true);
+        endVideo.SetActive(true);
     }
 
     public override void OnUpdate() {}
 
     public override void OnEnd() {
         endMenu.SetActive(false);
+    }
+
+    public void ShowEndMenu() {
+        endMenu.SetActive(true);
     }
     
 }
--- Vrij_II/Assets/01_Scripts/UI/VideoLoopEventHandler.cs
+++ Vrij_II/Assets/01_Scripts/UI/VideoLoopEventHandler.cs
@@ -1,20 +1,19 @@
+
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.Events;
 
-public class VideoLoopEventHandler : MonoBehaviour
-{
+public class VideoLoopEventHandler : MonoBehaviour {
+    
     public VideoPlayer videoPlayer;
     public UnityEvent OnLoopPointReached;
 
-    private void Start()
-    {
+    private void Start() {
         videoPlayer.loopPointReached += VideoLoopPointReached;
     }
 
-    private void VideoLoopPointReached(VideoPlayer vp)
-    {
+    private void VideoLoopPointReached(VideoPlayer vp) {
         OnLoopPointReached.Invoke();
-        gameObject.SetActive(false);
     }
+
 }
